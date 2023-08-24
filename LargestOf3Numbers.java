f6044afd1289005dc5668da9c577ba290b353f5b
Shivamkumar22525 <shivam22525@gmail.com>
2023-08-25 00:43:37 +0530

    Started my java journey

---
+++ LargestOf3Numbers.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class LargestOf3Numbers {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);{
+
+            System.out.print("a = ");
+            int a = sc.nextInt();
+
+            System.out.print("b = ");
+            int b = sc.nextInt();
+
+            System.out.print("c = ");
+             int c = sc.nextInt();
+
+            if (a>b && a>c){
+                System.out.println("a is greatest number");
+            } else if (b>a && b>c){
+                System.out.println("b is the greatest number");
+            }else{
+                System.out.println("c is the greatest number");
+            }
+
+        }
+    }
+
+}
