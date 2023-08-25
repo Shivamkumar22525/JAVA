4ce1933419fea976406937e068565dc5b25f502f
Shivamkumar22525 <shivam22525@gmail.com>
2023-08-25 12:15:03 +0530

    PositiveOrNegative by if else

---
+++ PositiveOrNegative.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class PositiveOrNegative {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);{
+
+            System.out.print("a = ");
+            int a = sc.nextInt();
+
+            if (a>=0){
+                System.out.println("Positive");
+            }
+            else {
+                System.out.println("Negative");
+            }
+
+        }
+    }
+}
