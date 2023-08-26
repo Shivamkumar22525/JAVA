de6d06b53427e2bf897ed4272f4c3c9bb4ac4d8f
Shivamkumar22525 <shivam22525@gmail.com>
2023-08-27 04:10:09 +0530

    do while loop

---
+++ Dowhile.java
@@ -0,0 +1,9 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.println("Shivam");
+            a++;
+        } while (a<=10);
+    }
+}
