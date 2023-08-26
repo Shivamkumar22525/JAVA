a9be0f60afd5f8cb446e5c2ea37c3bb7934e31fb
Shivamkumar22525 <shivam22525@gmail.com>
2023-08-27 00:49:21 +0530

    Leap year by if condition

---
+++ Sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int i = 1;
+
+        System.out.print("n = ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        while (i<=n){
+            sum = sum + i;
+
+            i++;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
