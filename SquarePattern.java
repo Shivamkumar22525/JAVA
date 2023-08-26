cfd3052d238ba58a2af6945e30d600d711912362
Shivamkumar22525 <shivam22525@gmail.com>
2023-08-27 01:27:38 +0530

    Revert "Revert "SUM by while loop""
    This reverts commit e84244c8db1f951870c21f760faa594ddaa746d2.

---
+++ SquarePattern.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i<=4; i++){
+            System.out.println("****");
+        }
+
+
+    }
+}
