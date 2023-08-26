b662b9b696c1828012a9abc4eadb2a9edecb26b9
Shivamkumar22525 <shivam22525@gmail.com>
2023-08-26 14:33:01 +0530

    check by ternary

---
+++ CheckbyTernary.java
@@ -0,0 +1,12 @@
+public class CheckbyTernary {
+    public static void main(String[] args) {
+
+        int a = 63, b = 36;
+
+        boolean x = (a < b) ? true : false;
+        System.out.println(x);
+
+        int y = (a > b) ? a : b;
+        System.out.println(y);
+    }
+}
