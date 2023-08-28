89076f873f8f4c4592a2937b3c080401f6412ffa
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-08-29 01:35:48 +0530

    reverse number by while and for loop

--- Reverse2.java
+++ Reverse2.java
@@ -4,24 +4,24 @@ public class Reverse2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-//        int rev = 0;
-//        int last;
-//
-//        int a = sc.nextInt();
-//
-//
-//        while (a>0){
-//            last = a%10;
-//            rev = (rev*10) + last; //reverse the number
-//            a = a/10;
-//        }
-//        System.out.println(rev);
+        int rev = 0;
+        int last;
 
-//        int rev = 0;
-//        for (int a = 12345; a>0;a/=10 ){
-//            int last = a%10;
-//             rev = (rev*10) + last;
-//        }
-//        System.out.println(rev);
+        int a = sc.nextInt();
+
+
+        while (a>0){
+            last = a%10;
+            rev = (rev*10) + last; //reverse the number
+            a = a/10;
+        }
+        System.out.println(rev);
+
+
+        for ( a = 12345; a>0;a/=10 ){
+             last = a%10;
+             rev = (rev*10) + last;
+        }
+        System.out.println(rev);
     }
 }
