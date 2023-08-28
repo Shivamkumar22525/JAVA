57f88f0c2edb3a68615f826ab4ea11699350c9c4
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-08-29 00:40:24 +0530

    multiple of 10 by for,while,do while loop

---
+++ Mulof10.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Mulof10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int max = Integer.MAX_VALUE;
+        int i = 0;
+
+
+
+//        for (int i = 0; i<=max; i++){
+//            System.out.print("Enter the number = ");
+//            int a = sc.nextInt();
+//            if (a%10 == 0){
+//                break;
+//            }
+//            System.out.println(a);
+//        }
+//        System.out.println("Can't print multiple of 10");
+
+
+//        while(i<=max){
+//            System.out.print("Enter the number = ");
+//            int a = sc.nextInt();
+//
+//            if (a%10 == 0){
+//                break;
+//            }
+//
+//            System.out.println(a);
+//        }
+//
+//        do {
+//            System.out.println("Enter the number = ");
+//            int a = sc.nextInt();
+//
+//            if (a%10 == 0){
+//                System.out.println("Sorry!!,it's a multiple of 10");
+//                break;
+//            }
+//        }while(i<=max);                // (i<=max) = true
+//this is example of git
+
+    }
+}
