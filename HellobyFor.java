dcff9200557015dd69418a0832f0b51873a9d406
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-08-29 15:03:16 +0530

    reverse number by while and for loop

---
+++ HellobyFor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HellobyFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i;
+        for (i=0;i<5;i++){
+            System.out.println("Hello");
+            i=i+2;
+        }
+    }
+}
