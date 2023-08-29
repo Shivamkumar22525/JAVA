5e0fa1160b4250e8b52856d4723092fe4b1daabe
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-08-30 04:51:12 +0530

    prime or not by loop

--- Prime.java
+++ Prime.java
@@ -7,19 +7,28 @@ public class Prime {
 
         System.out.print("Enter the number = ");
         int a = sc.nextInt();
-        int flag=0;
+        int flag = 1;
 
         for (int i = 2; i<=(a-1); i++){
+            flag = 0;
+            if(a == 1){
+                System.out.println("it's a natural number");
+                break;
+            }
+            if(a == 2){
+                System.out.println("number is prime");
+                break;
+            }
 
             if (a%i==0){
-                flag=1;
+                flag =1;
                 break;
             }
         }
-        if (flag == 0){
-            System.out.println("number is prime");
-        }else {
+        if (flag == 1){
             System.out.println("number is not prime");
+        }else {
+            System.out.println("number is prime");
         }
 
     }
