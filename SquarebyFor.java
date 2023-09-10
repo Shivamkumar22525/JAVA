79ddb4c7bdfae5c3313108c1c7159b64a0b2c125
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-09-10 12:05:50 +0530

    Square PATTERN by for loop

--- SquarebyFor.java
+++ SquarebyFor.java
@@ -4,8 +4,14 @@ public class SquarebyFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i<=4; i++){
-            System.out.println("****");
+        System.out.print("enter the number = ");
+        int num = sc.nextInt();
+
+        for (int i = 0; i<num; i++){
+            for (int j=0; j<num; j++){
+                System.out.print("*  ");
+            }
+            System.out.println();
         }
 
 
