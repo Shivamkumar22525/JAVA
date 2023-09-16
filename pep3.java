78c8b9a26f6ba8e3dbbc93fb826393cf5c948aeb
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-09-16 17:43:03 +0530

    pepcoding PATTERN-3 by for loop

--- pep3.java
+++ pep3.java
@@ -8,13 +8,28 @@ public class pep3 {
         int n = sc.nextInt();
 
         for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if (j==i){
+                    System.out.print("  *   ");
+                }else {
+                    System.out.print("      ");
+                }
 
-            for (int j=0; j<i; j++){
-                System.out.print("   ");
             }
-            for (int j=0; j<1;j++){
-                System.out.print(" * ");
+            for (int j=0; j<n; j++) {
+                if (j == (n - i-1)){
+                    System.out.print("  *   ");
+                }else {
+                    System.out.print("      ");
+                }
             }
+
+//            for (int j=0; j<n-i; j++){
+//                System.out.print("   ");
+//            }
+//            for (int j=0; j<1;j++){
+//                System.out.print(" * ");
+//            }
             System.out.println();
         }
     }
