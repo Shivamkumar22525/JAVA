dbd0344739a11aff8014f416c8e4d2929fbabfc6
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-10-15 03:30:57 +0530

    prime by function

--- F_prime.java
+++ F_prime.java
@@ -3,17 +3,26 @@ public class F_prime {
     public static boolean prime(int n){
         boolean check = true;
 
+        if(n==1){
+            return false;
+        }
+
+        if (n==2){
+            return check;
+        }
+
         for (int i = 2; i<n; i++){
             if (n%i==0){
                 check = false;
                 return check;
+
             }
         }
         return check;
     }
     public static void main(String[] args) {
 
-        System.out.println(prime(5));
+        System.out.println(prime(1));
 
     }
 }
