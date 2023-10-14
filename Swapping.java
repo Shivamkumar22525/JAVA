e79d3a046562274eaf93abea2219d4ef5352f83e
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-10-14 13:55:14 +0530

    swapping with functions

---
+++ Swapping.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Swapping {
+
+    public static void swap(int a,int b){
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("a = ");
+        int a = sc.nextInt();
+        System.out.print("b = ");
+        int b = sc.nextInt();
+        swap(a,b);
+    }
+}
