00b6c0778e45ffc0ae38af11d652c2c9dc57e13e
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-10-17 00:37:54 +0530

    decimal to binary by function

---
+++ DectoBi.java
@@ -0,0 +1,17 @@
+public class DectoBi {
+    public static int Dec(int n){
+        int pow = 0;
+        int bin = 0;
+        for (int i = n; i>0; i/=2){
+            int rem = i%2;
+            bin = bin+rem*(int)Math.pow(10,pow);
+            pow++;
+        }
+        return bin;
+    }
+    public static void main(String[] args) {
+
+        System.out.println(Dec(15));
+
+    }
+}
