f4b0af7005c0a0594b39c7f35e4dd26ea4f97859
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-10-17 23:13:35 +0530

    Average  by function

---
+++ Average.java
@@ -0,0 +1,12 @@
+public class Average {
+
+    public static int average(int a, int b, int c){
+        int average = (a+b+c)/3;
+        return average;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Average = " + average(23,34,56));
+
+    }
+}
