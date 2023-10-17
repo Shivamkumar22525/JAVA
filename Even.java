3adb2711b5baa047a11716abff13f31d3e5bfd18
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-10-18 01:05:10 +0530

    Check even by function

---
+++ Even.java
@@ -0,0 +1,11 @@
+public class Even {
+
+    public static boolean isEven(int n){
+        return n % 2 == 0;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(isEven(6));
+
+    }
+}
