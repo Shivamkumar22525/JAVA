0db80b3a0be6e205fcf20fd4bb28083cb409d782
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-10-18 01:45:23 +0530

    Palindrome by function

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+public class Palindrome {
+
+    public static void palindrome(int n){
+
+        int rev = 0;
+
+        for (int i = n; i>0; i/=10){
+            int LD = i%10;
+
+            rev = (rev*10)+LD;
+        }
+
+        if (n == rev){
+            System.out.println(n + " is a palindrome number");
+        }else {
+            System.out.println(n + " is not a palindrome number");
+        }
+
+    }
+    public static void main(String[] args) {
+
+        palindrome(169961);
+
+    }
+}
