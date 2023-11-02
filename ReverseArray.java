6fad7a5ddbdcfc61ffa604449257c768fd149902
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-11-02 09:40:19 +0530

    Reverse array

---
+++ ReverseArray.java
@@ -0,0 +1,25 @@
+public class ReverseArray {
+
+    public static void reverse(int[] num){
+        int start = 0, end = num.length-1;
+
+        while(start<end){
+            int temp = num[end];
+            num[end] = num[start];
+            num[start] = temp;
+
+            start++;
+            end--;
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6};
+
+        reverse(num);
+
+        for (int i = 0; i<num.length;i++){
+            System.out.print(num[i] + " ");
+        }
+    }
+}
