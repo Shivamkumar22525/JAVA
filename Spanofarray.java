adfdfcd65eb0b522de47a41aa51a3dcd7373ee76
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-12-05 01:42:47 +0530

    pep-8 array

---
+++ Spanofarray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Spanofarray {
+    public static int diff(int[] arr, int n){
+
+      int max = Integer.MIN_VALUE;
+      int min = Integer.MAX_VALUE;
+      for (int i = 0; i<arr.length; i++){
+          max = Math.max(max,arr[i]);
+          min = Math.min(min,arr[i]);
+      }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+
+      return max-min;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the range = ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter numbers = ");
+        for (int i = 0; i< arr.length; i++){
+          arr[i] = sc.nextInt();
+        }
+
+        int span = diff(arr,n);
+        System.out.println("span = " + span);
+    }
+}
