71167cc6b2dd8f99a99a641be4c6a21a4c2bb2d8
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2023-12-15 08:14:19 +0530

    pep-12 array

---
+++ DiffofArrays.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class DiffofArrays {
+
+    public static void diff(int[] arr1,int[] arr2){
+
+        for (int i = 0; i<arr1.length; i++){
+            System.out.print(arr1[i] - arr2[i] +",");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the length = ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter elements for arr1 = ");
+        int[] arr1 = new int[n];
+        for (int i = 0; i<arr1.length; i++){
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter elements for arr2 = ");
+        int[] arr2 = new int[n];
+        for (int i = 0; i<arr2.length; i++){
+            arr2[i] = sc.nextInt();
+        }
+
+        diff(arr1,arr2);
+
+
+    }
+}
