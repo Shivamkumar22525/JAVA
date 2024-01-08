97250bb7e684a3ff9c89275a3af98ca340cd3821
Shivamkumar22525 <143123681+Shivamkumar22525@users.noreply.github.com>
2024-01-09 01:46:15 +0530

    BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void Sort(int[] arr){
+
+        for (int i = 0; i<arr.length-1; i++){
+            int count = 0;
+            for (int j = 0; j<arr.length-1-i; j++){
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                    count++;
+                }
+
+            }
+            if (count==0) {
+                break;
+            }
+            System.out.print(count);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {1,2,3,4,5};
+        Sort(arr);
+
+        for (int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + ",");
+        }
+    }
+}
